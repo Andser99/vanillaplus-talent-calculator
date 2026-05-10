0d09ae293ea33f1696cb9009604a894696278bab
Andser <>
2026-05-10 13:03:11 +0200

    Added fallback to latest version if none is selected

--- src/TalentContext/versionProvider.ts
+++ src/TalentContext/versionProvider.ts
@@ -2,11 +2,12 @@ import exampleTree from "../trees/Druid/Balance.json"
 
 
 export function getTalentVersion() {
-    let version = parseInt(localStorage.getItem('talent-version') || '0') || 0;
+    let highest = Object.keys(exampleTree.Balance).length - 1;
+    let version = parseInt(localStorage.getItem('talent-version') || highest.toString()) || 0;
     console.log(`Current version: ${version}`);
-    if (localStorage.getItem('talent-version') === null || version > (Object.keys(exampleTree.Balance).length - 1)) {
+    if (localStorage.getItem('talent-version') === null || version > highest) {
     console.log(`No version found, setting to highest available.`);
-        setTalentVersion((Object.keys(exampleTree.Balance).length - 1).toString());
+        setTalentVersion(highest.toString());
     }
     return version;
 }
