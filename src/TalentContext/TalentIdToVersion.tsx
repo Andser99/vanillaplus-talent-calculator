cc7a403f0ab2e7c49ffe8361887f7aac2fcc7dbc
Andser <>
2026-07-17 19:04:07 +0200

    Added version description dropdown

--- src/TalentContext/TalentIdToVersion.tsx
+++ src/TalentContext/TalentIdToVersion.tsx
@@ -3,6 +3,7 @@ const IdToDate: Record<number, string> = {
   1: "UNKNOWN - Test",
   2: "2025-06-10",
   3: "2026-06-24",
+  4: "2026-07-17",
 };
 
 export function TalentIdToVersion(n: number): string | undefined {
